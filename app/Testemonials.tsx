6d26e4f48a9f6c7cebabb37cb29a84b9e5b5eb9b
Eleni001 <Eleni.SolomonSamuelsson@medieinstitutet.se>
2024-09-05 14:25:43 +0200

    add testemonials

---
+++ app/Testemonials.tsx
@@ -0,0 +1,155 @@
+"use client";
+
+import {
+  Avatar,
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Testimonial = (props: Props) => {
+  const { children } = props;
+
+  return <Box>{children}</Box>;
+};
+
+const TestimonialContent = (props: Props) => {
+  const { children } = props;
+
+  return (
+    <Stack
+      bg={useColorModeValue("white", "gray.800")}
+      boxShadow={"lg"}
+      p={8}
+      rounded={"xl"}
+      align={"center"}
+      pos={"relative"}
+      _after={{
+        content: `""`,
+        w: 0,
+        h: 0,
+        borderLeft: "solid transparent",
+        borderLeftWidth: 16,
+        borderRight: "solid transparent",
+        borderRightWidth: 16,
+        borderTop: "solid",
+        borderTopWidth: 16,
+        borderTopColor: useColorModeValue("white", "gray.800"),
+        pos: "absolute",
+        bottom: "-16px",
+        left: "50%",
+        transform: "translateX(-50%)",
+      }}
+    >
+      {children}
+    </Stack>
+  );
+};
+
+const TestimonialHeading = (props: Props) => {
+  const { children } = props;
+
+  return (
+    <Heading as={"h3"} fontSize={"xl"}>
+      {children}
+    </Heading>
+  );
+};
+
+const TestimonialText = (props: Props) => {
+  const { children } = props;
+
+  return (
+    <Text
+      textAlign={"center"}
+      color={useColorModeValue("gray.600", "gray.400")}
+      fontSize={"sm"}
+    >
+      {children}
+    </Text>
+  );
+};
+
+const TestimonialAvatar = ({ src, name }: { src: string; name: string }) => {
+  return (
+    <Flex align={"center"} mt={8} direction={"column"}>
+      <Avatar src={src} mb={2} />
+      <Stack spacing={-1} align={"center"}>
+        <Text fontWeight={600}>{name}</Text>
+      </Stack>
+    </Flex>
+  );
+};
+
+export default function Testemonials() {
+  return (
+    <Box bg={"#939598"}>
+      <Container maxW={"7xl"} py={16} as={Stack} spacing={12}>
+        <Stack spacing={0} align={"center"}>
+          <Heading>Our Clients Speak</Heading>
+          {/* <Text>We have been working with clients around the world</Text> */}
+        </Stack>
+        <Stack
+          direction={{ base: "column", md: "row" }}
+          spacing={{ base: 10, md: 4, lg: 10 }}
+        >
+          <Testimonial>
+            <TestimonialContent>
+              <TestimonialHeading>So Happy</TestimonialHeading>
+              <TestimonialText>
+                I am so happy that i found this place. Every time I leave my dog
+                there I know He is in good hands.
+              </TestimonialText>
+            </TestimonialContent>
+            <TestimonialAvatar
+              src={
+                "https://images.unsplash.com/photo-1583071299210-c6c113f4ac91?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fG1hbnxlbnwwfHwwfHx8Mg%3D%3D"
+              }
+              name={"David Cage"}
+            />
+          </Testimonial>
+          <Testimonial>
+            <TestimonialContent>
+              <TestimonialHeading>
+                I recommend Doggie Daycare
+              </TestimonialHeading>
+              <TestimonialText>
+                I can not honestly imagine a better place for my four legged
+                other than home with me.
+              </TestimonialText>
+            </TestimonialContent>
+            <TestimonialAvatar
+              src={
+                "https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80"
+              }
+              name={"Jane Cooper"}
+            />
+          </Testimonial>
+          <Testimonial>
+            <TestimonialContent>
+              <TestimonialHeading>The softest fur</TestimonialHeading>
+              <TestimonialText>
+                I just picked up my dog who just had a dog bath and I can not
+                get over how soft her fur is. Thank you!
+              </TestimonialText>
+            </TestimonialContent>
+            <TestimonialAvatar
+              src={
+                "https://images.unsplash.com/photo-1506707710000-189cf4c7f908?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTY4fHx3b21hbnxlbnwwfHwwfHx8Mg%3D%3D"
+              }
+              name={"Eva Peters"}
+            />
+          </Testimonial>
+        </Stack>
+      </Container>
+    </Box>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { Box } from "@chakra-ui/react";
 import Hero from "./Hero";
 import Services from "./Services";
+import Testemonials from "./Testemonials";
 
 export default function Home() {
   return (
     <Box>
       <Hero />
       <Services />
+      <Testemonials />
     </Box>
   );
 }
